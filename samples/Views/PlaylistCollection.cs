1c74bd09a8c4d57057d9b2ee146d3780e6e7a15f
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-12-24 16:59:09 -0600

    add responsiveness to example

--- samples/Views/PlaylistCollection.cs
+++ samples/Views/PlaylistCollection.cs
@@ -9,29 +9,35 @@ namespace Sample.Views;
 
 public class PlaylistCollection(DataAccessLayer dal) : FluidView
 {
+    private readonly GridItemsLayout _largeScreenLayout = new(ItemsLayoutOrientation.Vertical)
+    {
+        Span = 2,
+        HorizontalItemSpacing = 10,
+        VerticalItemSpacing = 10
+    };
+
+    private readonly LinearItemsLayout _smallScreenLayout = new(ItemsLayoutOrientation.Vertical)
+    {
+        ItemSpacing = 15
+    };
+
     public override View GetView()
     {
-        return new CollectionView()
+        var collectionView = new CollectionView
         {
-            ItemsLayout = DeviceInfo.Idiom == DeviceIdiom.Phone
-                ? new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
+            ItemsLayout = FlowNavigation.Current.View.ActiveBreakpoint >= BreakPoint.lg
+                ? _largeScreenLayout
+                : _smallScreenLayout
+        };
+
+        return collectionView
+            .ItemsSource(dal.Users)
+            .ItemTemplate(
+                TransitionView.Build<PlaylistCollection, Playlist, PlaylistTransitionView>(item =>
                 {
-                    ItemSpacing = 15
-                }
-                : new GridItemsLayout(ItemsLayoutOrientation.Vertical)
-                {
-                    Span = 2,
-                    HorizontalItemSpacing = 10,
-                    VerticalItemSpacing = 10
-                }
-        }
-        .ItemsSource(dal.Users)
-        .ItemTemplate(
-            TransitionView.Build<PlaylistCollection, Playlist, PlaylistTransitionView>(item =>
-            {
-                var user = (PlaylistVM)item;    // <- the item source
-                return $"id={user.Id}";         // <- the route params
-            }));
+                    var user = (PlaylistVM)item;    // <- the item source
+                    return $"id={user.Id}";         // <- the route params
+                }));
     }
 
     public override void OnEntering()
@@ -40,5 +46,24 @@ public class PlaylistCollection(DataAccessLayer dal) : FluidView
         StatusBar.SetColor(Colors.White);
         StatusBar.SetStyle(StatusBarStyle.DarkContent);
 #endif
+    }
+
+    protected override void OnBreakpointChanged()
+    {
+        // workaround for  https://github.com/dotnet/maui/issues/7747
+        Content = GetView();
+    }
+
+    // should work when the next issue is fixed:
+    // https://github.com/dotnet/maui/issues/7747
+
+    protected override void OnSm()
+    {
+        //_collectionView.ItemsLayout = _smallScreenLayout;
+    }
+
+    protected override void OnLg()
+    {
+        //_collectionView.ItemsLayout = _largeScreenLayout;
     }
 }
--- src/BreakPoint.cs
+++ src/BreakPoint.cs
@@ -1,10 +1,28 @@
 ﻿namespace FluidNav;
 
+/// <summary>
+/// Defines the screen breakpoints.
+/// </summary>
 public enum BreakPoint
 {
+    /// <summary>
+    /// 640px.
+    /// </summary>
     sm,
+    /// <summary>
+    /// 768px.
+    /// </summary>
     md,
+    /// <summary>
+    /// 1024px.
+    /// </summary>
     lg,
+    /// <summary>
+    /// 1280px.
+    /// </summary>
     xl,
+    /// <summary>
+    /// 1536px.
+    /// </summary>
     xxl
 }
